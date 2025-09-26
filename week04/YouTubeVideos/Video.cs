6e8df732ae52a6677bac5ebc4b89dafc9608e05d
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2025-09-26 20:27:36 -0300

    week04 more updates

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -5,7 +5,7 @@ public class Video
 {
     private string _title;
     private string _author;
-    private int _length; // in seconds
+    private int _length;
     private List<Comment> _comments;
 
     public Video(string title, string author, int length)
