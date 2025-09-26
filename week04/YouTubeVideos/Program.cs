609aa508c06511b95b54324c33bd207d8b3cdb51
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2025-09-26 20:26:38 -0300

    week04 updates

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,65 +1,11 @@
 using System;
 using System.Collections.Generic;
 
-class Comment
+public class Program
 {
-    public string CommenterName { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string commenterName, string text)
+    public static void Main()
     {
-        CommenterName = commenterName;
-        Text = text;
-    }
 
-    public void Display()
-    {
-        Console.WriteLine($"  {CommenterName}: {Text}");
-    }
-}
-
-class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Length { get; set; } // in seconds
-    private List<Comment> Comments { get; set; }
-
-    public Video(string title, string author, int length)
-    {
-        Title = title;
-        Author = author;
-        Length = length;
-        Comments = new List<Comment>();
-    }
-
-    public void AddComment(Comment comment)
-    {
-        Comments.Add(comment);
-    }
-
-    public int GetNumComments()
-    {
-        return Comments.Count;
-    }
-
-    public void Display()
-    {
-        Console.WriteLine($"\nTitle: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Length: {Length} seconds");
-        Console.WriteLine($"Number of Comments: {GetNumComments()}");
-        Console.WriteLine("Comments:");
-        foreach (Comment c in Comments)
-        {
-            c.Display();
-        }
-    }
-}
-class Program
-{
-    static void Main()
-    {
         Video v1 = new Video("Abstraction in C#", "Tech Guru", 600);
         Video v2 = new Video("OOP Basics", "Code Master", 720);
         Video v3 = new Video("Encapsulation Explained", "Dev Coach", 540);
